8cd96410b39cee88f1f90f2ac3a4a279be8a0d01
Tino Ehrich <tino.ehrich@hey.com>
2023-09-19 06:58:10 +0200

    added deno deploy

--- tasks/011-deno.sh
+++ tasks/011-deno.sh
@@ -18,3 +18,9 @@ echo "# deno" >> ~/.bashrc
 echo "" >> ~/.bashrc
 echo 'export DENO_INSTALL="/home/fightbulc/.deno"' >> ~/.bashrc
 echo 'export PATH="$DENO_INSTALL/bin:$PATH"' >> ~/.bashrc
+
+# deno deploy
+deno install -A --no-check -r -f https://deno.land/x/deploy/deployctl.ts
+echo "" >> ~/.bashrc
+echo "# deno deploy" >> ~/.bashrc
+echo "export DENO_DEPLOY_TOKEN=$DENO_DEPLOY_ACCESSTOKEN" >> ~/.bashrc
