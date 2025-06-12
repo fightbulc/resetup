82e294133ba1d7d2bd59234dd9d83cf484023789
Tino Ehrich <tino.ehrich@hey.com>
2025-06-12 17:33:24 +0200

    Fix rust recipe to run non-interactively
    Add -y flag to rustup installer to accept defaults and run without
    user interaction, fixing the test failure in CI.

--- recipes/rust.sh
+++ recipes/rust.sh
@@ -14,6 +14,5 @@ echo "- install rust"
 
 echo "" >> ~/.bashrc
 echo "# rust" >> ~/.bashrc
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
-source ~/.bashrc
